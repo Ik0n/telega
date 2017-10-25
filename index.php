ea1090a4e4923ec799e55dfabbf55804edadc62a
Ik0n <Ik0n16111998@gmail.com>
2017-10-26 01:27:19 +0300

    Testing System and learn new technology

--- index.php
+++ index.php
@@ -28,7 +28,8 @@ require_once('vendor/autoload.php');
     $result = pg_query($db, "SELECT id, name, about, refphoto
 	FROM public.\"Speakers\"
     WHERE ID = 1;");
-    var_dump(array($result));
+    $result = pg_fetch_assoc($result);
+    var_dump($result);
 
     $token = "466539344:AAE9QgFeHOxqWvJfEPgWcEXGDSvHj2qCZeM";
     $bot = new \TelegramBot\Api\Client($token);
