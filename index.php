8897162fc46ba2ddea7f9c8e04ec59626c1cc51e
Ik0n <Ik0n16111998@gmail.com>
2017-10-28 00:44:30 +0300

    Testing System and learn new technology

--- index.php
+++ index.php
@@ -147,14 +147,22 @@ require_once('vendor/autoload.php');
 
         if ($messageText == "30 ноября") {
             $db = pg_connect(pg_connection_string());
-            $results = pg_query($db, "SELECT title, begin, \"end\" FROM public.\"Schedule\";");
+            $results = pg_query($db, "SELECT id,title, begin, \"end\" FROM public.\"Schedule\";");
             $results = pg_fetch_all($results);
             $date = "30";
-
             foreach ($results as $result) {
                 $test = stristr($result['begin'], $date);
                 if ($test == $result['begin']) {
-                    $bot->sendMessage($message->getChat()->getId(), $result['title'] . " " . $result['begin'] . " " . $result['end']);
+                    $keyboard = new \TelegramBot\Api\Types\Inline\InlineKeyboardMarkup(
+                        [
+                            [
+                                ['callback_data' => $result['id'], 'text' => 'Добавить в своё расписание ' . $result['id']]
+                            ]
+                        ]
+                    );
+
+
+                    $bot->sendMessage($message->getChat()->getId(), $result['title'] . " " . $result['begin'] . " " . $result['end'], false, null, null, $keyboard);
                 }
             }
 
