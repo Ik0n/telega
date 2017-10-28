7bcbc615e0df77de1560dd06ca19d200aff99183
Ik0n <Ik0n16111998@gmail.com>
2017-10-28 18:26:52 +0300

    Testing System and learn new technology

--- index.php
+++ index.php
@@ -258,7 +258,7 @@ require_once('vendor/autoload.php');
       if ($data != null) {
           $db = pg_connect(pg_connection_string());
           $results = pg_query($db, "SELECT id, telegram_id FROM public.\"Users\" WHERE telegram_id =". $chatId . ";");
-          $results = pg_fetch_all($results);
+          $results = pg_fetch_assoc($results);
 
           pg_query($db, "INSERT INTO public.\"MySchedule\" (user_id, schedule_id) VALUES (". $results['id'] . "," . $data . ");");
           $bot->answerCallbackQuery($callback->getId(), "Added" . $data . " " . $chatId . " " . $fromId, true);
