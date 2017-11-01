c9c6ae7984d3eb737e74b6f064478b2b0ef0c87b
Ik0n <Ik0n16111998@gmail.com>
2017-11-01 17:16:23 +0300

    Testing System and learn new technology

--- speakers.php
+++ speakers.php
@@ -11,8 +11,15 @@
                 user=nhtxzmrecgoswb 
                 password=078e8a10351abf96961014d551717ef2b4fb31ce260b31ea5ebd24d3aff823b0 
                 sslmode=require");
-    $results = pg_query($db, "SELECT id, name, about, refphoto, session FROM public.\"Speakers\" ORDER BY id;");
-    $results = pg_fetch_all($results);
+    if (isset($_POST['submit'])) {
+        $data = $_POST;
+        pg_query($db, "INSERT INTO public.\"Speakers\"(
+	name, about, refphoto, session)
+	VALUES (". $data['name'] ."," . $data['about'] ."," . $data['refphoto'] . "," . $data['session'] . ");");
+
+        $results = pg_query($db, "SELECT id, name, about, refphoto, session FROM public.\"Speakers\" ORDER BY id;");
+        $results = pg_fetch_all($results);
+    }
 ?>
 <!doctype html>
 <html lang="en">
@@ -42,21 +49,21 @@
     <form action="speakers.php" method="POST">
         ФИО спикера
         <br>
-        <input type="text" name="name" title="name">
+        <input type="text" name="name" title="name" required>
         <hr>
         О спикере
         <br>
-        <input type="textarea" rows="10" cols="45" name="about" title="about">
+        <input type="textarea" rows="10" cols="45" name="about" title="about" required>
         <hr>
         Ссылка на фотографию спикера
         <br>
-        <input type="text" name="refphoto" title="refphoto">
+        <input type="text" name="refphoto" title="refphoto" required>
         <hr>
         Сессия в которой участвует спикер
         <br>
-        <input type="text" name="session" title="session">
+        <input type="text" name="session" title="session" required>
         <hr>
-        <input type="submit">
+        <input type="submit" name="submit">
     </form>
 </body>
 </html>
