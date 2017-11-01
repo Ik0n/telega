0fdcb9bb0a831cf2abf213560b21035c7bdca09c
Ik0n <Ik0n16111998@gmail.com>
2017-11-01 18:58:25 +0300

    Testing System and learn new technology

--- login.php
+++ login.php
@@ -7,7 +7,7 @@
  */
 
 if ($_POST['login'] == "admin" && $_POST['password'] == "admin") {
-    var_dump(time() . " " . date());
+    var_dump(time() . " " . date("DD.MM.YYYY"));
     echo "Вы вошли <br>";
     echo "<a href='speakers/index.php'>Спикеры</a>";
     echo "<a href='schedule/index.php'>Расписание</a>";
