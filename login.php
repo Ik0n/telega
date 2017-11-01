ec93f677387750aa42ab221ea8493e5a69a0f534
Ik0n <Ik0n16111998@gmail.com>
2017-11-01 15:34:33 +0300

    Testing System and learn new technology

---
+++ login.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Created by PhpStorm.
+ * User: Ik0n1
+ * Date: 01.11.2017
+ * Time: 15:31
+ */
+
+if ($_POST['login'] == "admin" && $_POST['password'] == "admin") {
+    echo "Вы вошли";
+}
