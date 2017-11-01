0501a613a3667f39e4397b00999c1c1fac7430c7
Ik0n <Ik0n16111998@gmail.com>
2017-11-01 19:28:02 +0300

    Testing System and learn new technology

--- login.php
+++ login.php
@@ -7,7 +7,7 @@
  */
 
 if ($_POST['login'] == "admin" && $_POST['password'] == "admin") {
-    $test = date("H:i:s", "3");
+    $test = date("H:i:s", time() + 10800);
     var_dump(date("Y-m-d") . " " . $test);
     echo "Вы вошли <br>";
     echo "<a href='speakers/index.php'>Спикеры</a>";
