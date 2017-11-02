3c620eb275911ae8c1f1fc6e33310108871c048d
Ik0n <Ik0n16111998@gmail.com>
2017-11-03 01:42:15 +0300

    Testing System and learn new technology

--- index.php
+++ index.php
@@ -43,13 +43,6 @@ require_once('TelegramBot.php');
  $token = "466539344:AAE9QgFeHOxqWvJfEPgWcEXGDSvHj2qCZeM";
  $bot = new \TelegramBot\Api\Client($token);
 
- if (!file_exists("registered.trigger")) {
-     $page_url = "https://" . $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
-     $result = $bot->setWebhook("https://bottelegabot.herokuapp.com/");
-     if($result) {
-         file_put_contents("registered.trigger", time());
-     }
- }
     $bot->command('start', function ($message) use ($bot) {
         $answer = 'Что я могу для вас сделать?';
 
