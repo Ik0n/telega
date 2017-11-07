5e6af9a2e51fbcb1ed187982f829b515f25db53a
Ik0n <Ik0n16111998@gmail.com>
2017-11-08 01:56:25 +0300

    Testing System and learn new technology

--- speakers/index.php
+++ speakers/index.php
@@ -20,7 +20,7 @@
             $fileTempName = $_FILES['filename']['tmp_name'];
 
             move_uploaded_file($fileTempName, $uploadfile);
-            pg_query($db, "INSERT INTO public.\"Speakers\"(name, about, refphoto, session) VALUES ('". $data['name'] ."','" . $data['about'] ."','" . "https://bottelegabot.herokuapp.com/images/" . $data['filename'] . "','" . $data['session'] . "');");
+            pg_query($db, "INSERT INTO public.\"Speakers\"(name, about, refphoto, session) VALUES ('". $data['name'] ."','" . $data['about'] ."','" . "https://bottelegabot.herokuapp.com/images/" . $_FILES['filename']['name'] . "','" . $data['session'] . "');");
         } else {
             pg_query($db, "INSERT INTO public.\"Speakers\"(name, about, refphoto, session) VALUES ('". $data['name'] ."','" . $data['about'] ."','" . $data['refphoto'] . "','" . $data['session'] . "');");
         }
